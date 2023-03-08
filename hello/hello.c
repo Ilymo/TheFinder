221e690a89112e118813c0c1ef50bbed24c4aee0
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-03-08 14:27:01 +0000

    Wed, Mar 8, 2023, 3:27 PM +01:00

--- hello/hello.c
+++ hello/hello.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
-    get_string("What's your name? \n");
-    printf("hello, world\n");
+    string name = get_string("What's your name? ");
+    printf("hello, %s\n", name);
 }
