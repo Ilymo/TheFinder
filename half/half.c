70438fefa4f581c595ba2ab89dcb5b619bd572e2
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-03-09 18:06:27 +0000

    Thu, Mar 9, 2023, 7:06 PM +01:00

--- half/half.c
+++ half/half.c
@@ -18,8 +18,8 @@ int main(void)
 // TODO: Complete the function
 float half(float bill, float tax, int tip)
 {
-
-
+    tax = bill * (tax / 100);
+    tip = (bill + tax) * (tip / 100);
 
 
     float total = (bill + tax + tip) / 2;
