ec49bf8bdd1b74faf4035498d21651a5d55bafd0
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-03-09 17:04:25 +0000

    Thu, Mar 9, 2023, 6:04 PM +01:00

--- debug/debug.c
+++ debug/debug.c
@@ -6,9 +6,9 @@
 int main(void)
 {
     // Ask for your name and where live
-    name = get_string("What is your name? ")
-    location = get_string("Where do you live? ")
+    string name = get_string("What is your name? ");
+    string location = get_string("Where do you live? ");
 
     // Say hello
-    print("Hello, %i, from %i!", name, location)
+    printf("Hello, %s, from %s!", name, location);
 }
