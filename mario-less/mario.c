5ed336c458860afeab65416ce5755162c3960d5d
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-03-09 12:27:20 +0000

    Thu, Mar 9, 2023, 1:27 PM +01:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,7 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    //Ask for height
+    int height;
+    do
+    {
+        height = get_int("height: \n");
+    }
+    while (1 <= height <= 8);
+    //
 }
