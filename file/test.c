2239b348bb15e954e53b4c12213cc2b7afc84a80
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-03-09 08:36:58 +0000

    Thu, Mar 9, 2023, 9:36 AM +01:00

--- file/test.c
+++ file/test.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main ( void)
+int main (void)
 
 {
     string name = get_string("What's your name? ");
     int age = get_int("How old are you? ");
 
 
-    int
-    printf("Hello %s, happy to see you.\n", name);
+    int n = 30
+    int delta = (30 - age)
+    for (delta > 0)
+    {
+        printf = ("you are %i from 30 years old.\n", delta);
+    }
+    else
+    {
+        printf = (")
+    }
+      printf("Hello %s, happy to see you.\n", name);
 }
