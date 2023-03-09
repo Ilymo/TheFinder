24bdd6e5ab8a3a3d62df5c13d42d7327361ba417
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-03-09 12:50:41 +0000

    Thu, Mar 9, 2023, 1:50 PM +01:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -15,11 +15,9 @@ int main(void)
 
     for (int i = 0; i < n; i++)
     {
-        for (int j = 1; j < i; j++)
+        for (int j = 0; j < i; j++)
         printf("#");
-        printf("\n");
+
     }
-
-
-
+    printf("\n");
 }
