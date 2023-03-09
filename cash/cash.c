bc3d080be321c190ce06a5edd25f61e05adfff8f
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-03-09 16:14:41 +0000

    Thu, Mar 9, 2023, 5:14 PM +01:00

--- cash/cash.c
+++ cash/cash.c
@@ -10,12 +10,7 @@ int calculate_pennies(int cents);
 int main(void)
 {
     // Ask how many cents the customer is owed
-    do
-    {
-        int cents = get_cents("Number of cents: ");
-    }
-    while (cents < 1)
-
+    int cents = get_cents();
 
     // Calculate the number of quarters to give the customer
     int quarters = calculate_quarters(cents);
