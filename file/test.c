12f92aa57d7ca58271320730669ad9855149e11c
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-03-09 08:30:55 +0000

    Thu, Mar 9, 2023, 9:30 AM +01:00

---
+++ file/test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main ( void)
+
+{
+    string name = get_string("What's your name? ");
+    int age = get_int("How old are you? ");
+
+
+    int
+    printf("Hello %s, happy to see you.\n", name);
+}
--- test.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <cs50.h>
-
-int main ( void)
-
-{
-    string Name = get_string("What's your name? \n");
-}
