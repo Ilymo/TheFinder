59fb8f6a5db2c2ca292eebfe7796a85667fd0c97
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-11-20 09:58:23 +0000

    Mon, Nov 20, 2023, 10:58 AM +01:00

---
+++ readability/readability.c
@@ -0,0 +1,38 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+
+int count_letters(string text);
+
+int main(void)
+{
+    //prompt user for text
+    string text = get_string("Text: ");
+    int letters = count_letters(text);
+
+    printf("%i\n", letters);
+
+}
+
+    //count letters
+int count_letters(string text)
+{
+    int n_letters = 0;
+
+    for (int i = 0, len = strlen(text); i < len; i++)
+    {
+        if (islower(text[i]) || isupper(text[i]))
+        {
+            n_letters++;
+        }
+    }
+    return n_letters;
+}
+
+
+
+        //count words
+    //count sentences
+    //print "Grade: (X)"; "Grade: 16+" if higher than 16; "Before grade 1" if lower than 1
