3d2f213bcd8dd42c695a1152b0a8f925b16d44e4
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-11-20 17:39:05 +0000

    Mon, Nov 20, 2023, 6:39 PM +01:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -4,16 +4,12 @@
 int main(int argc, string argv[])
 {
     // Make sure getting 1 argument (not 0 else 2+: return 1
-    if (argc != 1)
+    if (argc == 1)
     {
-        printf("Usage: ./caesar key\n");
-        return 1;
+        return 0;
     }
-    else
-    {
-        
-    }
-
+    printf("Usage: ./caesar key\n");
+    return 1;
     //Check all argv[x] are digits (print error if not)
 
     //stock argv[x] in K
