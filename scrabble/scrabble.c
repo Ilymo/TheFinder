b20233700ee8172773189218062b69a647ba3128
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-11-20 08:25:32 +0000

    Mon, Nov 20, 2023, 9:25 AM +01:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -32,6 +32,10 @@ int compute_score(string word)
     for (int i = 0, int len = strlen(word); i < len; i++)
     {
         if (isupper(word[i]))
+        {
+            score += SCORE[i] - "A";
+        }
+        
     }
 
 
