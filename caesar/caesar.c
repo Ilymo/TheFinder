c505e22d25f6d48a31020d92359edccea3d6d8b2
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-11-20 17:31:04 +0000

    Mon, Nov 20, 2023, 6:31 PM +01:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,7 +1,20 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
+    // Make sure getting 1 argument (not 0 else 2+: return 1
+
+    //Check all argv[x] are digits (print error if not)
+
+    //stock argv[x] in K
+
+    //get plaintext
+
+    //for each chara in plaintexte
+
+        //rotate chara if it's a letter with K
+
+    //print cyphertext
 
 }
