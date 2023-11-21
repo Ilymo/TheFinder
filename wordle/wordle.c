ed84cdafa292f57f83e7edddcb585806de6101ba
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-11-21 16:33:29 +0000

    Tue, Nov 21, 2023, 5:33 PM +01:00

--- wordle/wordle.c
+++ wordle/wordle.c
@@ -89,6 +89,7 @@ int main(int argc, string argv[])
 
         // set all elements of status array initially to 0, aka WRONG
         // TODO #4
+        
 
         // Calculate score for the guess
         int score = check_word(guess, wordsize, status, choice);
