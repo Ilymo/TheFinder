f2932a333b9db7b5de203ab2f64912d1ab2a5389
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-11-21 10:05:53 +0000

    Tue, Nov 21, 2023, 11:05 AM +01:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -3,14 +3,23 @@
 
 int main(int argc, string argv[])
 {
-
+    // get the key
+    if (argc != 2)
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
 }
 
-// get the key
+
 // validate key
     // check length
     // check only lettter
     //check no repeated characters
 // prompt for plaintext
+
 // encipher
+    //for each character, determinate what letter it map to
+    // preserve case
+    // leave non-alphabetique like this
 // Print ciphertext
