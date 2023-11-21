90aaabef9886eb307240198254b38b5384c64366
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-11-21 10:01:18 +0000

    Tue, Nov 21, 2023, 11:01 AM +01:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,7 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
 
 }
+
+// get the key
+// validate key
+// prompt for plaintext
+// encipher
+// Print ciphertext
