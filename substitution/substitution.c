805496900eb33b9e3315e8d222d4a6e2ca1dfff7
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-11-21 10:13:39 +0000

    Tue, Nov 21, 2023, 11:13 AM +01:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,5 +1,10 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+
+bool valid_key(string s);
 
 int main(int argc, string argv[])
 {
@@ -13,9 +18,34 @@ int main(int argc, string argv[])
 
 
 // validate key
-    // check length
-    // check only lettter
-    //check no repeated characters
+bool valid_key(string s);
+{
+    bool validation = true;
+    for (int i = 0, len = strlen(s); i < len; i++)
+    {
+        // check length
+        if (len = 25)
+        {
+            // check only lettter
+            if (isalpha(s[i]))
+            {
+                //check no repeated characters
+            }
+            else
+            {
+                valiation = false;
+            }
+        }
+        else
+        {
+            validation = false
+        }
+    }
+    return valiation;
+}
+
+
+
 // prompt for plaintext
 
 // encipher
