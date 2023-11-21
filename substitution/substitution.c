e9e40dcb6db95fb95d9aaf3c1f7d10d87275ee33
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-11-21 11:01:35 +0000

    Tue, Nov 21, 2023, 12:01 PM +01:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -42,7 +42,7 @@ bool valid_key(string s)
                     {
                         if (validation == true)
                         {
-                            if (key[n] != s[i + 1])
+                            if (key[n] != s[n + 1])
                             {
                                 validation = true;
                             }
