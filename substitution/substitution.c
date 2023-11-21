6bcecfd992645a433e1dc7b3a3cdf3166615930b
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-11-21 10:02:28 +0000

    Tue, Nov 21, 2023, 11:02 AM +01:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -8,6 +8,9 @@ int main(int argc, string argv[])
 
 // get the key
 // validate key
+    // check length
+    // check only lettter
+    //check no repeated characters
 // prompt for plaintext
 // encipher
 // Print ciphertext
