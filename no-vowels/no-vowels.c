9e418e4522a5b8675f2e868401767f57abc32ebe
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-11-21 18:25:21 +0000

    Tue, Nov 21, 2023, 7:25 PM +01:00

--- no-vowels/no-vowels.c
+++ no-vowels/no-vowels.c
@@ -6,7 +6,38 @@
 #include <cs50.h>
 #include <stdio.h>
 
+string replace(string);
+
 int main(int argc, string argv[])
 {
-    
+    if (argc != 2)
+    {
+        printf("Usage: ./no-vowels word\n");
+        return 1;
+    }
+}
+
+
+string replace(string s)
+{
+    for (int i = 0, len = strlen(s); i < len ; i++)
+    {
+        if (s[i] == 'a')
+        {
+            s[i] = '6';
+        }
+        else if (s[i] == 'e')
+        {
+            s[i] = '3';
+        }
+        else if (s[i] == 'i')
+        {
+            s[i] = '1';
+        }
+        else if (s[i] == 'o')
+        {
+            s[i] = '0';
+        }
+    }
+    return s;
 }
