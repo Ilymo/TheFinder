fea89b3cd1734d3af8d4f31e189a9e803ecdae82
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-11-21 15:30:21 +0000

    Tue, Nov 21, 2023, 4:30 PM +01:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -5,7 +5,7 @@
 
 
 bool valid_key(string s);
-char cipher(char c);
+char cipher(char c, int key);
 
 int main(int argc, string argv[])
 {
@@ -32,7 +32,7 @@ int main(int argc, string argv[])
     //print each character rotated
     for (int i = 0, len = strlen(P); i < len; i++)
     {
-        printf("%c", cipher(P[i]));
+        printf("%c", cipher(P[i], key));
     }
     printf("\n");
 }
@@ -40,7 +40,7 @@ int main(int argc, string argv[])
 
 
 // encipher
-char cipher (char c)
+char cipher (char c, int key)
 {
     int stock;
     if (isalpha(c))
