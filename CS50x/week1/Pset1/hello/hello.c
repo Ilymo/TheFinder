bee2bc114212c8767a71cae6ddf27d751be91e80
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-11-22 13:17:46 +0000

    Wed, Nov 22, 2023, 2:17 PM +01:00

--- CS50x/week1/Pset1/hello/hello.c
+++ CS50x/week1/Pset1/hello/hello.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
 
 int main(void)
 {
     string name = get_string("What's your name? "); // Ask for the name
-    printf("hello, %s\n", name); //Print the name on the screen
+    printf("hello, %s\n", name);                    // Print the name on the screen
 }
