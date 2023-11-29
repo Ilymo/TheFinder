1783c02b9eef180fa5cf5d88fc45d93aa45b15a7
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-11-29 14:00:44 +0000

    Wed, Nov 29, 2023, 3:00 PM +01:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -70,7 +70,7 @@ bool vote(string name)
     //compare name with each name in array and add 1vote if finded, else return false
     for (int i = 0; i < candidate_count; i++)
     {
-        if (strcasecmp(name, candidates[i].name))
+        if (strcasecmp(name, candidates[i].name) == 0)
         {
             candidates[i].votes++;
             return true;
