931a4c9eba39bf6c451198761db214fedd6bc60e
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-11-29 14:42:17 +0000

    Wed, Nov 29, 2023, 3:42 PM +01:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -96,7 +96,7 @@ void print_winner(void)
             }
             else
             {
-                return;
+                break;
             }
         }
         printf("%s \n", candidates[winners[i]].name);
