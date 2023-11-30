ed9c0fb16ced3f75e2148e3dad26fcdbab2d5267
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-11-30 11:50:18 +0000

    Thu, Nov 30, 2023, 12:50 PM +01:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -178,6 +178,7 @@ bool print_winner(void)
         //if more than candidate_count/2 return true
         if (candidates[i].votes > (voter_count/2))
         {
+            printf("%s\n", candidates[i].name);
             return true;
         }
     }
