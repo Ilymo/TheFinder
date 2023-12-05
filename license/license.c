e1ed816ef06d559f29755aa7e419b315231767a4
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-05 18:33:29 +0000

    Tue, Dec 5, 2023, 7:33 PM +01:00

--- license/license.c
+++ license/license.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
     // Create array to store plate numbers
     char *plates[8];
 
-    char *holder[8];
+    char holder[8];
 
     FILE *infile = fopen(argv[1], "r");
 
