ffa38b6d6b35a7ca7002b3ed86b479e4f1217df6
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-06 11:00:36 +0000

    Wed, Dec 6, 2023, 12:00 PM +01:00

--- volume/volume.c
+++ volume/volume.c
@@ -40,6 +40,7 @@ int main(int argc, char *argv[])
 
     // TODO: Read samples from input file and write updated data to output file
     int16_t buffer;
+    fread(&buffer, 1, )
 
     // Close files
     fclose(input);
