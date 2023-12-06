fb2363cbcac3ace8786ba5197c32eb8cc1da141b
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-06 15:44:19 +0000

    Wed, Dec 6, 2023, 4:44 PM +01:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -61,7 +61,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j <= (width / 2); j++)
+        for (int j = 0; j < (width / 2); j++)
         {
             RGBTRIPLE tmp = image[i][j];
             image[i][j] = image[i][width - j - 1];
