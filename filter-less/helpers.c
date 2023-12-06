6b655cdc1cb60469dab6d581cc992d519e61414e
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-06 14:18:57 +0000

    Wed, Dec 6, 2023, 3:18 PM +01:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -27,14 +27,27 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-                float sepiaRed = .393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue
-                if (sepiaRed > '255')
-                {
-                    sepiaRed = 255;
-                }
-                image[i][j].rgbtRed = round(sepiaRed);
-                image[i][j].rgbtBlue = rounded;
-                image[i][j].rgbtGreen = rounded;
+
+            float sepiaRed = .393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue;
+            if (sepiaRed > 255)
+            {
+                sepiaRed = 255;
+            }
+            image[i][j].rgbtRed = round(sepiaRed);
+
+            float sepiaGreen = .349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue;
+            if (sepiaGreen > 255)
+            {
+                sepiaGreen = 255;
+            }
+            image[i][j].rgbtGreen = round(sepiaGreen);
+
+            float sepiaBlue = .272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue;
+            if (sepiaBlue > 255)
+            {
+                sepiaBlue = 255;
+            }
+            image[i][j].rgbtBlue = round(sepiaBlue);
 
         }
     }
