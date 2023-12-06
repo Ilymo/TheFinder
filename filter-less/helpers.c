66d3a426012ea76b16c254159451c38f77b76d0d
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-06 14:10:16 +0000

    Wed, Dec 6, 2023, 3:10 PM +01:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -23,6 +23,17 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+                
+                image[i][j].rgbtRed = rounded;
+                image[i][j].rgbtBlue = rounded;
+                image[i][j].rgbtGreen = rounded;
+
+        }
+    }
     return;
 }
 
