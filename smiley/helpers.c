94641655fda7fc7be73d6319938a72b21eef1ef6
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-06 09:07:38 +0000

    Wed, Dec 6, 2023, 10:07 AM +01:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -11,6 +11,11 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
         {
             RGBTRIPLE pixel = image[i][j];
             printf("%i; %i; %i\n", pixel.rgbtBlue, pixel.rgbtRed, pixel.rgbtGreen);
+            if (pixel.rgbtBlue == 0)
+            {
+                pixel.rgbtBlue = 100;
+            }
+            printf("%i; %i; %i\n", pixel.rgbtBlue, pixel.rgbtRed, pixel.rgbtGreen);
         }
     }
 }
