91cd034dddc66b24ff3981c3c4cca777c9cfb6f3
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-06 09:01:27 +0000

    Wed, Dec 6, 2023, 10:01 AM +01:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -1,6 +1,18 @@
 #include "helpers.h"
+#include <stdio.h>
 
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
     // Change all black pixels to a color of your choosing
+
+    for (int i = 0; i < width; i++)
+    {
+        for (int j = 0; j < height; j++)
+        {
+            RGBTRIPLE pixel = image[i][j];
+            printf("%i\n", pixel.rgbtBlue);
+            printf("%i\n", pixel.rgbtRed);
+            printf("%i\n", pixel.rgbtGreen);
+        }
+    }
 }
