7bceff2cb1457d1c82b94368cdb8c3ffa8dbfd69
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-06 07:48:27 +0000

    Wed, Dec 6, 2023, 8:48 AM +01:00

--- license/license.c
+++ license/license.c
@@ -21,10 +21,14 @@ int main(int argc, char *argv[])
 
     int idx = 0;
 
+    
+
     while (fread(buffer, 1, 7, infile) == 7)
     {
         // Replace '\n' with '\0'
         buffer[6] = '\0';
+
+
 
         // Save plate number in array
         plates[idx] = buffer;
