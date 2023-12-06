3d607bc8d0886924f5317d319f10c81a9af23de7
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-06 10:59:05 +0000

    Wed, Dec 6, 2023, 11:59 AM +01:00

--- volume/volume.c
+++ volume/volume.c
@@ -35,7 +35,8 @@ int main(int argc, char *argv[])
 
     // TODO: Copy header from input file to output file
     uint8_t header[HEADER_SIZE];
-    fread(&h)
+    fread(&header, 1, HEADER_SIZE; input);
+    fwrite(&header, 1, HEADER_SIZE, output);
 
     // TODO: Read samples from input file and write updated data to output file
     int16_t buffer;
