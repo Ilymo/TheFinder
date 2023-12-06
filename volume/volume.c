cd4bb81abdbdcb3d4b69f45f07f3e01deec02774
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-06 10:55:12 +0000

    Wed, Dec 6, 2023, 11:55 AM +01:00

--- volume/volume.c
+++ volume/volume.c
@@ -34,6 +34,9 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
+    uint8_t header[HEADER_SIZE];
+    int16_t buffer;
+    fread()
 
     // TODO: Read samples from input file and write updated data to output file
 
