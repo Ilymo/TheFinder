8682208f0f2ea7f993dea819f24e5f20b988cb86
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-06 09:13:26 +0000

    Wed, Dec 6, 2023, 10:13 AM +01:00

--- test.c
+++
@@ -1,7 +0,0 @@
-            RGBTRIPLE pixel = image[i][j];
-            printf("%i; %i; %i\n", pixel.rgbtBlue, pixel.rgbtRed, pixel.rgbtGreen);
-            if (pixel.rgbtBlue == 0)
-            {
-                pixel.rgbtBlue = 100;
-                image[i][j] = pixel;
-            }
