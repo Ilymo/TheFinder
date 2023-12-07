05c23f97788b489ff9d66a5bca8759f028c670f4
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-07 12:42:45 +0000

    Thu, Dec 7, 2023, 1:42 PM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -21,13 +21,20 @@ int main(int argc, char *argv[])
     // Create buffer for reading
     int BLOCK_SIZE = 512;
     int buffer[BLOCK_SIZE];
-    int n = 0;
+    int count = 0;
     char *filename;
 
+    // read 512 byte into memory
     while (fread(buffer, 1, BLOCK_SIZE, infile) == BLOCK_SIZE)
     {
+        // if start of new jpeg
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
+            //if first jpeg
+            if (count == 0)
+            {
+                
+            }
 
                         sprintf(filename, "%03i.jpg", n);
                         filename = malloc(BLOCK_SIZE);
