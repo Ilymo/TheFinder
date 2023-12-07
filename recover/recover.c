cb32bfc8bf2646ad236f1a8adb449969bceef008
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-07 10:35:09 +0000

    Thu, Dec 7, 2023, 11:35 AM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+    if (argc != 2)
+    {
+        printf("Usage: ./recover IMAGE\n");
+        return 1;
+    }
+
 }
