4bc9c65fbedaa3d5cef6d10ba4faf2da1bec407e
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-07 13:58:36 +0000

    Thu, Dec 7, 2023, 2:58 PM +01:00

--- reverse/reverse.c
+++ reverse/reverse.c
@@ -11,6 +11,11 @@ int main(int argc, char *argv[])
 {
     // Ensure proper usage
     // TODO #1
+    if (argc != 3)
+    {
+        printf("Usage: ./reverse input.wav output.wav\n");
+        return 1;
+    }
 
     // Open input file for reading
     // TODO #2
