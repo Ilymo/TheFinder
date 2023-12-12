8c2cd5dd2c44cba8a8fafe303a2dd4291f1b8071
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-12 17:18:07 +0000

    Tue, Dec 12, 2023, 6:18 PM +01:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -84,9 +84,8 @@ void free_family(person *p)
     }
 
     // TODO: Free parents recursively
-    free(p->parents[0]);
-    free(p->parents[1]);
-    free_family(p);
+    free_family(p->parents[0]);
+    free_family(p->parents[1]);
 
     // TODO: Free child
     free(p);
