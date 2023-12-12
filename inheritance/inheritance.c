12ec21a7296f0ea2f6958219eb2a54e02a0857b2
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-12 16:27:45 +0000

    Tue, Dec 12, 2023, 5:27 PM +01:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -39,7 +39,7 @@ int main(void)
 person *create_family(int generations)
 {
     // TODO: Allocate memory for new person
-    
+    person *p = malloc(sizeof(person));
 
     // If there are still generations left to create
     if (generations > 1)
