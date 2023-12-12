338d3665d777f7eafcd21490399c240110bf8ea8
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-12 17:19:02 +0000

    Tue, Dec 12, 2023, 6:19 PM +01:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -88,7 +88,8 @@ void free_family(person *p)
     free_family(p->parents[1]);
 
     // TODO: Free child
-    free(p);
+    free(p->parents[0]);
+    free(p->parents[1]);
 }
 
 // Print each family member and their alleles.
