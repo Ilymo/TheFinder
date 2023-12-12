d024b3639fdebc8ecbd7865faaa896385dc12a3a
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-12 17:22:57 +0000

    Tue, Dec 12, 2023, 6:22 PM +01:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -78,7 +78,7 @@ person *create_family(int generations)
 void free_family(person *p)
 {
     // TODO: Handle base case
-    if (p->parents[0] == NULL)
+    if (p == NULL)
     {
         return;
     }
