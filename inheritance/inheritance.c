4cdd5e122efcd9da6cbbe694ad187b439899a0a5
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-12 16:35:13 +0000

    Tue, Dec 12, 2023, 5:35 PM +01:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -49,8 +49,11 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
+        p.parents[0] = parent0;
+        p.parents[1] = parent1
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
+        
     }
 
     // If there are no generations left to create
