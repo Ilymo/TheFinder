f5005a41fbb3553f8047e9dce3bdc31696173c89
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-15 16:59:59 +0000

    Fri, Dec 15, 2023, 5:59 PM +01:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -2,32 +2,28 @@
 
 from cs50 import get_int
 
-#get height
+
+# get height
 def main():
     height = get_height()
     printp(height)
 
 
-
-
-
-
-
-
-#print space
+# print space
 def printp(n):
     row = n
     for i in range(n):
-        print(' ' * (row - 1), end='')
-        print('#' * (i + 1))
+        print(" " * (row - 1), end="")
+        print("#" * (i + 1))
         row -= 1
 
 
+# get height from user
 def get_height():
-        while True:
-            n = get_int("height: ")
-            if n > 0 and n < 9:
-                return n
+    while True:
+        n = get_int("height: ")
+        if n > 0 and n < 9:
+            return n
 
 
 main()
