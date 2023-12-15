3f7d9b3636c76617197b4e672820c3fee2630dd7
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-15 17:42:23 +0000

    Fri, Dec 15, 2023, 6:42 PM +01:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -12,18 +12,18 @@ def main():
 def nb_coin(i):
     nb = 0
     #calcul quarters and remaining change
-    q = i / 0.25
+    q = round(i / 0.25)
     i -= q * 0.25
     #calcul dimes
-    d = i / 0.10
+    d = round(i / 0.10)
     i -= d * 0.10
     #calcul nickels
-    n = i / 0.05
+    n = round(i / 0.05)
     i -= n * 0.05
     #calcul pennies
-    p = i / 0.01
-    nb += int(p + n + d + q)
+    p = round(i / 0.01)
 
+    nb += p + n + d + q
     return nb
 
 
