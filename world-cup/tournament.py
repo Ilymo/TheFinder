61e2ee5addf8b6bb00511023cd4692a5649bab95
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-15 13:07:50 +0000

    Fri, Dec 15, 2023, 2:07 PM +01:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -18,7 +18,7 @@ def main():
     # TODO: Read teams into memory from file
     with open(sys.argv[1]) as file:
             reader = csv.DictReader(file)
-            teams = reader
+            teams.append(reader)
             print(f"{teams}")
 
 
