52e5c7a33cd531f927b7b3dabddf8cf229a9b80b
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-15 13:47:45 +0000

    Fri, Dec 15, 2023, 2:47 PM +01:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -58,7 +58,7 @@ def simulate_round(teams):
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
     # TODO
-    for i in range(4):
+    for i in range():
         teams = simulate_round(teams)
 
 
