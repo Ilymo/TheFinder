dc4369c4d5937d7aee467acc2151dc1876baca22
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-15 17:17:25 +0000

    Fri, Dec 15, 2023, 6:17 PM +01:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -5,18 +5,23 @@ form cs50 import get_float
 
 def main():
     change = get_change()
+    coin = nb_coin(change)
 
 
 
 
 
 
+def nb_coin(n):
+    
 
 
 
 def get_change():
-    n = get_float("Change owed: ")
-
+    while True:
+        n = get_float("Change owed: ")
+        if n > 0:
+            return n
 
 
 
