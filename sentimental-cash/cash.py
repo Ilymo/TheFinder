be4396a03966e2b3f1628a0f627c5fa0e74bc6db
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-15 18:08:18 +0000

    Fri, Dec 15, 2023, 7:08 PM +01:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -16,7 +16,7 @@ def nb_coin(i):
     nb = 0
     #calcul quarters and remaining change
     q = floor(i / 0.25)
-    i = Decimal(0.41) - Decimal(0.25)
+    i += q * Decimal(0.25)
     #calcul dimes
     d = floor(i / 0.10)
     i -= d * 0.10
