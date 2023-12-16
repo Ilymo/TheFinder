16969e023271fddb0f720f35d360b20e73b23a3e
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-16 12:44:52 +0000

    Sat, Dec 16, 2023, 1:44 PM +01:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -15,9 +15,20 @@ def lts(text):
 
 # calculate grade
 def calcul_grade(letters, words, sentences):
-    
+    L = letters / words * 100
+    S = sentences / words * 100
+    grade = in(0.0588 * L - 0.296 * S - 15.8)
+    return grade
+
 
 # print grade
+def print_grade(grade):
+    if grade >= 16:
+        print("Grade 16+")
+    elif grade < 1:
+        print("Before Grade 1")
+    else:
+        print(grade)
 
 # func count letter
 
