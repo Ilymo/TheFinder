c9b7175996f4de711233fc7d4a39110bf7ed7245
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-16 13:17:35 +0000

    Sat, Dec 16, 2023, 2:17 PM +01:00

--- sentimental-readability/test.py
+++ sentimental-readability/test.py
@@ -1,6 +1,8 @@
 
 text = "oliVier Mor elLi"
 nb = 0
+letters = 0
+spaces = 0
 
 #count letter
 for char in text:
@@ -10,7 +12,7 @@ for char in text:
         spaces += 1
 
 #calcul word
-   nb = letters / spaces
+nb = letters / spaces
 
 
 print(nb)
