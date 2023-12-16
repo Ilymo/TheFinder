644abdb353853bd367b163c11245ae1fc92d7760
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-16 10:44:20 +0000

    Sat, Dec 16, 2023, 11:44 AM +01:00

--- sentimental-credit/test.py
+++ sentimental-credit/test.py
@@ -6,9 +6,9 @@ sum = 0
 length = len(n)
 
 #add 0,2,4... to sum
-for i in range(length):
+for i in range(length - 1):
     sum += int(n[i]) * 2
-    i += 2
+    i += 3
 
 
     #multi other by 2 and add to sum
