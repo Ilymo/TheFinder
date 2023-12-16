405a5e3b884a354d2a33bf5aec446ae195112093
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-16 10:57:24 +0000

    Sat, Dec 16, 2023, 11:57 AM +01:00

--- sentimental-credit/test.py
+++ sentimental-credit/test.py
@@ -7,7 +7,9 @@ length = len(n)
 
 #add 0,2,4... to sum
 for i in range(0, length, 2):
-    sum += int(n[i]) * 2
+    holder += int(n[i]) * 2
+    for n in range(len(holder)):
+        sum += holder[n]
 
 
     #multi other by 2 and add to sum
