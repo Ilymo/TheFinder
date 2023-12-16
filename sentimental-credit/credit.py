e165854ee95748f98676a006967a2abcdbb7963d
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-16 12:28:38 +0000

    Sat, Dec 16, 2023, 1:28 PM +01:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -2,71 +2,69 @@
 from cs50 import get_int
 
 
-
 def main():
     number = get_card_nb()
     result = check_valid(number)
     print(result)
 
 
-#prompt for credit card nb and check length
+# prompt for credit card nb and check length
 def get_card_nb():
     n = input("number: ")
     return n
 
 
-#check if valid, if yes return the type
+# check if valid, if yes return the type
 def check_valid(n):
-    #check AMEX
-    if n[0] == '3':
-        if n[1] in ['4', '7']:
+    # check AMEX
+    if n[0] == "3":
+        if n[1] in ["4", "7"]:
             if len(n) == 15:
                 amex = luhn(n)
                 if amex == 0:
-                    return 'AMEX'
+                    return "AMEX"
                 else:
-                    return 'INVALID'
-    #check MASTER
-    elif n[0] == '5':
-        if n[1] in ['1', '2', '3', '4', '5']:
+                    return "INVALID"
+    # check MASTER
+    elif n[0] == "5":
+        if n[1] in ["1", "2", "3", "4", "5"]:
             if len(n) == 16:
                 master = luhn(n)
                 if master == 0:
-                    return 'MASTERCARD'
+                    return "MASTERCARD"
                 else:
-                    return 'INVALID'
-    #check VISA
-    elif n[0] == '4':
+                    return "INVALID"
+    # check VISA
+    elif n[0] == "4":
         if len(n) == 13 or len(n) == 16:
             visa = luhn(n)
             if visa == 0:
-                return 'VISA'
+                return "VISA"
             else:
-                return 'INVALID'
+                return "INVALID"
 
-    return 'INVALID'
+    return "INVALID"
 
 
-#Luhn's algo
+# Luhn's algo
 def luhn(n):
     sum = 0
     length = len(n)
     holder = 0
 
-    #add 0,2,4... to sum
+    # add 0,2,4... to sum
     for i in range(length - 2, -1, -2):
         holder = int(n[i]) * 2
         holder = str(holder)
         for k in range(len(holder)):
             sum += int(holder[k])
 
-    #multi other by 2 and add to sum
+    # multi other by 2 and add to sum
     for j in range(length - 1, -1, -2):
         sum += int(n[j])
 
-
-    #return last digit
-    return(sum % 10)
+    # return last digit
+    return sum % 10
 
 
 main()
