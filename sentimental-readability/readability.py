b4d6b5ca153a05e9f7e6179f64a4d846b77c6b4c
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-16 12:40:02 +0000

    Sat, Dec 16, 2023, 1:40 PM +01:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -2,11 +2,20 @@
 from cs50 import get_string
 
 # prompt user for text
-
+def get_text():
+    n = get_string("text: ")
+    return n
 
 # return letters, words, sentences
+def lts(text):
+    letters = count_letter(text)
+    words = count_word(text)
+    sentences = count_sentence(text)
+    return(letters, words, sentences)
 
 # calculate grade
+def calcul_grade(letters, words, sentences):
+    
 
 # print grade
 
