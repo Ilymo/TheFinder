35a9490a55d3e823b9f719d0656e941108f04e74
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-16 12:05:45 +0000

    Sat, Dec 16, 2023, 1:05 PM +01:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -54,14 +54,14 @@ def luhn(n):
     holder = 0
 
     #add 0,2,4... to sum
-    for i in range(length - 1, 0, 2):
+    for i in range(length, 0, 2):
         holder = int(n[i]) * 2
         holder = str(holder)
         for k in range(len(holder)):
             sum += int(holder[k])
 
     #multi other by 2 and add to sum
-    for j in range(length - 1, 0, 2):
+    for j in range(length, 1, 2):
         sum += int(n[j])
 
 
