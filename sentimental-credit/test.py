877200feee46683e307f0712009856beb1a21916
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-16 11:05:05 +0000

    Sat, Dec 16, 2023, 12:05 PM +01:00

--- sentimental-credit/test.py
+++ sentimental-credit/test.py
@@ -4,13 +4,13 @@ n = "4003600000000014"
 
 sum = 0
 length = len(n)
-holder = "0"
+holder = 0
 
 #add 0,2,4... to sum
 for i in range(0, length, 2):
-    holder = str(int(n[i]) * 2)
-    for n in range(len(holder)):
-        sum += int(holder[n])
+    holder = int(n[i]) * 2
+    holder = str(holder)
+
 
 
     #multi other by 2 and add to sum
