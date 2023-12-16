789214a7e5d11910abdb2b6fbceb75209214cc56
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-16 09:07:54 +0000

    Sat, Dec 16, 2023, 10:07 AM +01:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,5 +1,27 @@
 # TODO
 
+
+def main():
+
+
+
+
+
+
+
+
+
 #prompt for credit card nb
+def get_card_nb():
+    
 
 
+#check if valid
+def check_valid(n):
+
+
+#if valid, check if AMEX, MASTERCARD or VISA
+def check_type(n):
+
+
+main()
