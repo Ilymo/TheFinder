e2a02621a49e56a04c77fd157b1070e9ecef46db
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-16 12:07:34 +0000

    Sat, Dec 16, 2023, 1:07 PM +01:00

--- sentimental-credit/test.py
+++ sentimental-credit/test.py
@@ -1,5 +1,5 @@
 
-n = "378282246310005"
+n = "4222222222223"
 
 
 sum = 0
@@ -7,7 +7,7 @@ length = len(n)
 holder = 0
 
 #add 0,2,4... to sum
-for i in range(0, length, 2):
+for i in range(length, 0, 2):
     holder = int(n[i]) * 2
     holder = str(holder)
     for k in range(len(holder)):
@@ -16,7 +16,7 @@ for i in range(0, length, 2):
 
 
 #multi other by 2 and add to sum
-for j in range(1, length, 2):
+for j in range(length, 1, 2):
     sum += int(n[j])
 
 
