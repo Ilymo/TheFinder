5f8e43b90afc09cae1d1aaeb8421dac78bcf4ab7
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-16 13:11:20 +0000

    Sat, Dec 16, 2023, 2:11 PM +01:00

--- sentimental-readability/test.py
+++ sentimental-readability/test.py
@@ -3,8 +3,10 @@ text = "oliVier Mor elLi"
 nb = 0
 
 
-if text[0].isallpha():
+if text[0].isalpha():
     nb += 1
-    
+    for char in text:
+        if char.isalpha:
+            nb += 1
 
 print(nb)
