d780a52637f6586e8d3daa66f03c2c57cade14ce
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-18 07:32:14 +0000

    Mon, Dec 18, 2023, 8:32 AM +01:00

--- dna/dna.py
+++ dna/dna.py
@@ -9,7 +9,9 @@ def main():
         print("Usage: python dna.py data.csv sequence.txt")
 
     # TODO: Read database file into a variable
-    
+    with open(sys.argv[1]) as file:
+        reader = csv.DictReader(file)
+        
 
     # TODO: Read DNA sequence file into a variable
 
