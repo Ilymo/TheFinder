6b81e5282b920db7c0ade618622783bf86b71611
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-19 13:59:28 +0000

    Tue, Dec 19, 2023, 2:59 PM +01:00

--- movies/4.sql
+++ movies/4.sql
@@ -1,1 +1,1 @@
-SELECT COUNT() FROM movies WHERE id IN (SELECT movie_id FROM ratings WHERE rating = 10);
+SELECT COUNT(title) FROM movies WHERE id IN (SELECT movie_id FROM ratings WHERE rating = 10);
