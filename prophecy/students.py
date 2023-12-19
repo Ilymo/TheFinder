e9c0243afd7834351ac8bbc4d61c62205561f2b2
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-19 10:37:18 +0000

    Tue, Dec 19, 2023, 11:37 AM +01:00

--- prophecy/students.py
+++ prophecy/students.py
@@ -17,11 +17,8 @@ with open("students.csv", "r") as file:
         #db.execute("INSERT INTO students(id, student_name) VALUES(?, ?)", row["id"], row["student_name"])
 
         # insert id, house_name and house_head into houses table
-        try:
-            db.execute("INSERT INTO houses(id, house_name, house_head) VALUES (?, ?, ?)", id, row["house"], row["head"])
-            id += 1
-        except ValueError or RuntimeError:
-            pass
+        db.execute("INSERT INTO houses(id, house_name, house_head) VALUES (?, ?, ?)", id, row["house"], row["head"])
+        id += 1
 
 
 
