27672eb60302b94544321293e49b3ceed22f45c5
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-19 10:03:11 +0000

    Tue, Dec 19, 2023, 11:03 AM +01:00

--- prophecy/students.py
+++ prophecy/students.py
@@ -17,8 +17,11 @@ with open("students.csv", "r") as file:
         #db.execute("INSERT INTO students(id, student_name) VALUES(?, ?)", row["id"], row["student_name"])
 
         # insert id, house_name and house_head into houses table
-        if row["house"] != 
-        db.execute("INSERT INTO houses(id, house_name, house_head) VALUES (?, ?, ?)", id, row["house"], row["head"])
+        house = db.execute("SELECT house_name FROM houses")
+        if row["house"] == house:
+            continue
+        else:
+            db.execute("INSERT INTO houses(id, house_name, house_head) VALUES (?, ?, ?)", id, row["house"], row["head"])
         id += 1
 
 
