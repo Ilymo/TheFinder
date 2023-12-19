c0d43da5f110f01048518f60a940849cf412b724
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-19 13:26:52 +0000

    Tue, Dec 19, 2023, 2:26 PM +01:00

--- songs/6.sql
+++ songs/6.sql
@@ -1,1 +1,1 @@
-SELECT * FROM songs WHERE artist_id = (SELECT id FROM artists WHERE name = Post Malone )
+SELECT name FROM songs WHERE artist_id = (SELECT id FROM artists WHERE name = "Post Malone");
