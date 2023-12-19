a89b46dd6f7472ac9b2b04796bc015e5a40ed98d
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-19 09:37:44 +0000

    Tue, Dec 19, 2023, 10:37 AM +01:00

--- prophecy/students.py
+++ prophecy/students.py
@@ -12,8 +12,12 @@ with open("students.csv", "r") as file:
     reader = csv.DictReader(file)
     for row in reader:
         print(row)
-        # insert ID in student table
-        db.execute("INSERT INTO students(id, student_name) VALUES(?, ?)", row["id"], row["student_name"])
+        # insert ID and name into students table
+        #db.execute("INSERT INTO students(id, student_name) VALUES(?, ?)", row["id"], row["student_name"])
+
+        # insert id, house_name and house_head into houses table
+        id = 1
+        db.execute("INSERT INTO houses(id, house_name, house_head) VALUES (?, ?, ?)", id, row["house"], row["head"])
 
 
 
