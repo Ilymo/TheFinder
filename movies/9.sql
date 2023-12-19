a4e1ed3298ac9e52f32e3b6e430a997bcc8f72bd
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-19 14:39:32 +0000

    Tue, Dec 19, 2023, 3:39 PM +01:00

--- movies/9.sql
+++ movies/9.sql
@@ -1,1 +1,1 @@
-SELECT * FROM people WHERE id in (SELECT person_id FROM stars WHERE movie_id in (SELECT id FROM movies WHERE year = "2004")) ORDER BY year
+SELECT DISTINCT(name) FROM people WHERE id in (SELECT person_id FROM stars WHERE movie_id in (SELECT id FROM movies WHERE year = "2004")) ORDER BY birth
