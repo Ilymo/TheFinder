de5d7f8d1d316f2ebc7c777a592b5e4d2f599a8b
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-19 16:11:05 +0000

    Tue, Dec 19, 2023, 5:11 PM +01:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,2 +1,2 @@
-SELECT * FROM (SELECT * FROM stars WHERE person_id = (SELECT id FROM people WHERE name = "Bradley Cooper")) WHERE person_id = (SELECT id FROM people WHERE name = "Jennifer Laurence")
+SELECT * FROM star(SELECT person_id FROM stars WHERE movie_id IN (SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = "Bradley Cooper")))
 
