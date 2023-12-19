235deb62b82a453aad009dd01901b88e93865d47
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-19 14:35:53 +0000

    Tue, Dec 19, 2023, 3:35 PM +01:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,1 +1,1 @@
-SELECT title, rating FROM movies JOIN ratings ON movies.id = ratings.movie_id  WHERE year = "2010" AND rating > 0 ORDER BY rating DESC
+SELECT title, rating FROM movies JOIN ratings ON movies.id = ratings.movie_id  WHERE year = "2010" AND rating > 0 ORDER BY rating DESC, title ASC;
