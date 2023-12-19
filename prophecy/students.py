73c175f5abc3ee417e417a77e08de5b9c3067ffd
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-19 09:18:52 +0000

    Tue, Dec 19, 2023, 10:18 AM +01:00

--- prophecy/students.py
+++ prophecy/students.py
@@ -4,10 +4,16 @@ import csv
 # Open csv file
 with open("students.csv", "r") as file:
 
+# open database
+db1 = SQL("sqlite:///roster.db")
+db2 = SQL("sqlite:///new.db")
+
+
+
     #creat dict reader
     reader = csv.DictReader(file)
     for row in reader:
-        print(row)
+
 
 
 
