cc876c395078a6b06bfe473440d1eecf4da506e6
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-19 18:12:20 +0000

    Tue, Dec 19, 2023, 7:12 PM +01:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -60,5 +60,7 @@ WHERE id IN
     )
 AND license_plate IN
     (
-    
+    SELECT license_plate
+    FROM bakery_security_logs
+    WHERE year = "2021" AND month = "7" AND day = "28" AND hour = "10" AND minute >= "15" AND minute <= "30"
     )
