c0df468765165dfa3da5248faff57cb5a33583bf
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-19 17:40:54 +0000

    Tue, Dec 19, 2023, 6:40 PM +01:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,9 +1,9 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
 -- check the crime report
-SELECT *
-FROM crime_scene_reports
-WHERE year = "2021" AND month = "7" AND day = "28" AND street = "Humphrey Street"
+--SELECT *
+--FROM crime_scene_reports
+--WHERE year = "2021" AND month = "7" AND day = "28" AND street = "Humphrey Street"
 -- something about bakery on humphrey street
 
 -- check bakery_security_logs
@@ -11,3 +11,6 @@ WHERE year = "2021" AND month = "7" AND day = "28" AND street = "Humphrey Street
 --FROM bakery_security_logs
 --WHERE year = "2021" AND month = "7" AND day = "28"
 
+SELECT *
+FROM interviews
+WHERE year = "2021" AND month = "7" AND day = "28"
