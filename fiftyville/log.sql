96a74ed377c8c709450cacbd247b9d2aae43e148
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-19 17:52:08 +0000

    Tue, Dec 19, 2023, 6:52 PM +01:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -9,7 +9,7 @@
 -- check bakery_security_logs
 SELECT *
 FROM bakery_security_logs
-WHERE year = "2021" AND month = "7" AND day = "28"
+WHERE year = "2021" AND month = "7" AND day = "28" AND hour = "10" AND minute >= "15" AND minute <= "30"
 
 -- check interviews
 --SELECT *
