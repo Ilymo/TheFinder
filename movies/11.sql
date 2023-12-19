3af5478c21e2df260b0395fa4ed9679f8cedbdbc
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-19 15:43:25 +0000

    Tue, Dec 19, 2023, 4:43 PM +01:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,2 +1,3 @@
-SELECT * FROM movies WHERE id IN (SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = "Chadwick Boseman"))
+--SELECT * FROM movies WHERE id IN (SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = "Chadwick Boseman"))
 
+SELECT title, rating FROM movies JOIN ratings ON movies.id = ratings.movie_id
