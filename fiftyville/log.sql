5389d382f3d366cd287aa9e8dd0bc9af7bbce62c
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-20 07:27:42 +0000

    Wed, Dec 20, 2023, 8:27 AM +01:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -45,22 +45,27 @@
     --)
 
 --Take id check people with id from atm->bank_acount AND plate from BakerySecLog
+--SELECT *
+--FROM people
+--WHERE id IN
+    --(
+    --SELECT person_id
+    --FROM bank_accounts
+    --WHERE account_number IN
+        --(
+        --SELECT account_number
+        --FROM atm_transactions
+        --WHERE year = "2021" AND month = "7" AND day = "28" AND atm_location = "Leggett Street"
+        --)
+    --)
+--AND license_plate IN
+    --(
+    --SELECT license_plate
+    --FROM bakery_security_logs
+    --WHERE year = "2021" AND month = "7" AND day = "28" AND hour = "10" AND minute >= "15" AND minute <= "30"
+    --)
+
+-- Check flight
 SELECT *
-FROM people
-WHERE id IN
-    (
-    SELECT person_id
-    FROM bank_accounts
-    WHERE account_number IN
-        (
-        SELECT account_number
-        FROM atm_transactions
-        WHERE year = "2021" AND month = "7" AND day = "28" AND atm_location = "Leggett Street"
-        )
-    )
-AND license_plate IN
-    (
-    SELECT license_plate
-    FROM bakery_security_logs
-    WHERE year = "2021" AND month = "7" AND day = "28" AND hour = "10" AND minute >= "15" AND minute <= "30"
-    )
+FROM flights
+
