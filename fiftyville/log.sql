b52e0532e8c87d3e3110397d79e1d59b0b187a65
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-20 07:49:19 +0000

    Wed, Dec 20, 2023, 8:49 AM +01:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -78,19 +78,16 @@
 
 -- thief flight goes from fiftyville to LaGuardia Airport - New York City
 
--- check phone calls where duration <= 60s
---SELECT *
---FROM phone_calls
---WHERE year = "2021" AND month = "7" AND day = "28" AND duration <= "60"
-
-
--- check passenger where passport nb in ID from bank_account
+-- check phone calls where duration <= 60s AND hone_number in ID from bank account
 SELECT *
-FROM passengers
-WHERE flight_id = "36"
-AND passport_number IN
+FROM phone_calls
+WHERE year = "2021"
+AND month = "7"
+AND day = "28"
+AND duration <= "60"
+AND caller IN
     (
-    SELECT passport_number
+    SELECT phone_number
     FROM people
     WHERE id IN
         (
@@ -110,3 +107,32 @@ AND passport_number IN
         WHERE year = "2021" AND month = "7" AND day = "28" AND hour = "10" AND minute >= "15" AND minute <= "30"
     )
     )
+
+
+-- check passenger where passport nb in ID from bank_account
+--SELECT *
+--FROM passengers
+--WHERE flight_id = "36"
+--AND passport_number IN
+    --(
+--    SELECT passport_number
+--    FROM people
+  --  WHERE id IN
+  --      (
+    --    SELECT person_id
+      --  FROM bank_accounts
+        --WHERE account_number IN
+          --  (
+            --SELECT account_number
+--            FROM atm_transactions
+  --          WHERE year = "2021" AND month = "7" AND day = "28" AND atm_location = "Leggett Street"
+    --        )
+      --  )
+    --AND license_plate IN
+      --  (
+        --SELECT license_plate
+        --FROM bakery_security_logs
+        --WHERE year = "2021" AND month = "7" AND day = "28" AND hour = "10" AND minute >= "15" AND minute <= "30"
+    --)
+    --)
+
