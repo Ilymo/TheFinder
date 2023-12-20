dbe5a1d1fcdb177b72e84a1e8ffc42fb5c996a8f
Ilymo <118273904+Ilymo@users.noreply.github.com>
2023-12-20 07:42:13 +0000

    Wed, Dec 20, 2023, 8:42 AM +01:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -84,7 +84,29 @@
 --WHERE year = "2021" AND month = "7" AND day = "28" AND duration <= "60"
 
 
--- check passenger
+-- check passenger where passport nb in ID from bank_account
 SELECT *
 FROM passengers
 WHERE flight_id = "36"
+AND passport_number IN
+    (
+    SELECT *
+--FROM people
+--WHERE id IN
+    --(
+    --SELECT person_id
+    --FROM bank_accounts
+    --WHERE account_number IN
+        --(
+        --SELECT account_number
+        --FROM atm_transactions
+        --WHERE year = "2021" AND month = "7" AND day = "28" AND atm_location = "Leggett Street"
+        --)
+    --)
+--AND license_plate IN
+    --(
+    --SELECT license_plate
+    --FROM bakery_security_logs
+    --WHERE year = "2021" AND month = "7" AND day = "28" AND hour = "10" AND minute >= "15" AND minute <= "30"
+    --)
+    )
