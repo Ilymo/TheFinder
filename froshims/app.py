31ffc0a9df9eb1a9a196f5880d61e66cc06dd208
Ilymo <118273904+Ilymo@users.noreply.github.com>
2024-01-08 16:11:04 +0000

    Mon, Jan 8, 2024, 5:11 PM +01:00

--- froshims/app.py
+++ froshims/app.py
@@ -2,11 +2,8 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def index():
-    if request.method == "POST":
-        name = request.form.get("name")
-        return render_template("greet.html", name=name)
     return render_template("index.html")
 
 
