5d919f386ff863a9eb0fa75305f95118f5e746b7
Ilymo <118273904+Ilymo@users.noreply.github.com>
2024-01-08 14:39:51 +0000

    Mon, Jan 8, 2024, 3:39 PM +01:00

--- lecture9/app.py
+++ lecture9/app.py
@@ -4,11 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    if request_methods == "POST"
+        name = request.form.get("name", "weirdo")
+        return render_template("greet.html", name=name)
     return render_template("index.html")
 
-
-@app.route("/greet", methods=["POST"])
-def greet():
-    name = request.form.get("name", "weirdo")
-    return render_template("greet.html", name=name)
 
