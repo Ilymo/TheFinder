e7d0645ea7bb83a529f30db6df5ca30f22282b24
Ilymo <118273904+Ilymo@users.noreply.github.com>
2024-01-08 14:42:33 +0000

    Mon, Jan 8, 2024, 3:42 PM +01:00

--- lecture9/app.py
+++ lecture9/app.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"]
+@app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == "POST"
+    if request.method == "POST":
         name = request.form.get("name", "weirdo")
         return render_template("greet.html", name=name)
     return render_template("index.html")
