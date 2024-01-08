e95aa2cdd574d0dde09200b0157411c5afb73ad4
Ilymo <118273904+Ilymo@users.noreply.github.com>
2024-01-08 14:32:07 +0000

    Mon, Jan 8, 2024, 3:32 PM +01:00

--- lecture9/app.py
+++ lecture9/app.py
@@ -7,7 +7,7 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/greet")
+@app.route("/greet", methods=["POST"])
 def greet():
     name = request.args.get("name", "weirdo")
     return render_template("greet.html", name=name)
