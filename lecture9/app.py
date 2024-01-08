134d43dff99cd64a8951e022c0e243c3ad1cbc9f
Ilymo <118273904+Ilymo@users.noreply.github.com>
2024-01-08 12:10:53 +0000

    Mon, Jan 8, 2024, 1:10 PM +01:00

--- lecture9/app.py
+++ lecture9/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/greet")
 def index():
     name = request.args.get("name", "weirdo")
     return render_template("index.html", name=name)
