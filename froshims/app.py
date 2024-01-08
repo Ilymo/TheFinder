ed4d4b9909c6e0cccdb0605f8c5b2aecb1c0c8b2
Ilymo <118273904+Ilymo@users.noreply.github.com>
2024-01-08 16:27:38 +0000

    Mon, Jan 8, 2024, 5:27 PM +01:00

--- froshims/app.py
+++ froshims/app.py
@@ -8,6 +8,7 @@ def index():
 
 @app.route("/register", methods=["POST"])
 def register():
-    if not request.form.get("name"):
-        return "failure"
+    if not request.form.get("name") or not request.form.get("sport"):
+        return render_template("failure.html")
+    return render_template("succes.html")
 
