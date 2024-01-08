67d8930146071c6ec5b2ebd411ba46f307deab33
Ilymo <118273904+Ilymo@users.noreply.github.com>
2024-01-08 16:22:32 +0000

    Mon, Jan 8, 2024, 5:22 PM +01:00

--- froshims/app.py
+++ froshims/app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/register")
+@app.route("/register", methods=["POST"])
 def register():
     if not request.form.get("name"):
         return "failure"
-    
+
