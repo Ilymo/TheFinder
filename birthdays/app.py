427663a92e7083fe560d48f1c65e3c8e6f7e3f34
Ilymo <118273904+Ilymo@users.noreply.github.com>
2024-01-11 16:21:31 +0000

    Thu, Jan 11, 2024, 5:21 PM +01:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -33,7 +33,7 @@ def index():
     else:
 
         # TODO: Display the entries in the database on index.html
-        
+        birthday=request.args.get
 
         return render_template("index.html")
 
