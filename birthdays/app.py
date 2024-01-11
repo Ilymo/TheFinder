c4fa157d4a74d1f9cbdb1c19eb3b05ec433d5898
Ilymo <118273904+Ilymo@users.noreply.github.com>
2024-01-11 16:43:42 +0000

    Thu, Jan 11, 2024, 5:43 PM +01:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -36,7 +36,7 @@ def index():
     else:
 
         # TODO: Display the entries in the database on index.html
-        birthday = db.execute("SELECT * FROM birthdays")
+        birthdays = db.execute("SELECT * FROM birthdays")
         return render_template("index.html", birthdays=birthdays)
 
 
