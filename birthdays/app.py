7cfb0e9253631189a45346fe1a899c49ec78857c
Ilymo <118273904+Ilymo@users.noreply.github.com>
2024-01-11 16:42:08 +0000

    Thu, Jan 11, 2024, 5:42 PM +01:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -27,7 +27,10 @@ def index():
     if request.method == "POST":
 
         # TODO: Add the user's entry into the database
-
+        name = request.form.get("name")
+        month = request.form.get("month")
+        day = request.form.get("day")
+        db.execute("INSERT INTO birthdays (name, month, day) VALUES(?, ?, ?)", name, month, day)
         return redirect("/")
 
     else:
