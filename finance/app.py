ce2549c8c1e3300842b0fb91968abf0d4ddbb3f1
Ilymo <118273904+Ilymo@users.noreply.github.com>
2024-01-31 10:24:03 +0000

    Wed, Jan 31, 2024, 11:24 AM +01:00

--- finance/app.py
+++ finance/app.py
@@ -106,7 +106,14 @@ def logout():
 @login_required
 def quote():
     """Get stock quote."""
-    return apology("TODO")
+    if request.method == "POST":
+        symbol = request.form.get("symbol")
+        print(symbol)
+        stock = lookup("symbol")
+        print(stock)
+        return render_template("quoted.html", stock="stock")
+    else:
+        return render_template("quote.html")
 
 
 @app.route("/register", methods=["GET", "POST"])
