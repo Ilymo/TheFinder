80a1ae057b4e4f7e9b59352fcb597e5a734f7e77
Ilymo <118273904+Ilymo@users.noreply.github.com>
2024-02-01 08:11:14 +0000

    Thu, Feb 1, 2024, 9:11 AM +01:00

--- finance/app.py
+++ finance/app.py
@@ -73,12 +73,12 @@ def buy():
             # Check if symbol already in holding with this user_id
             holding = db.execute("SELECT * FROM holding WHERE user_id = ? GROUP BY symbol", session["user_id"])
             print(holding)
-            if stock["symbol"] in holding:
+            if stock["symbol"] in holding[0]["symbol"]:
                 db.execute("UPDATE holding SET shares = shares + ? WHERE user_id = ? AND symbol = ?",
                         shares, session["user_id"], stock["symbol"])
-
-            db.execute("INSERT INTO holding (user_id, symbol, shares) VALUES (?, ?, ?)",
-                       session["user_id"], stock["symbol"], shares)
+            else:
+                db.execute("INSERT INTO holding (user_id, symbol, shares) VALUES (?, ?, ?)",
+                            session["user_id"], stock["symbol"], shares)
             # calcule new user cash and update users table
             newcash = user[0]["cash"] - (stock["price"] * shares)
             db.execute("UPDATE users SET cash = ? WHERE id = ?", newcash, session["user_id"])
