3522824d96b0f9b20df4e46db9293623b2e875f5
Ilymo <118273904+Ilymo@users.noreply.github.com>
2024-02-06 08:44:58 +0000

    Tue, Feb 6, 2024, 9:44 AM +01:00

---
+++ final-project/app.py
@@ -0,0 +1,10 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+
+# Configure application
+app = Flask(__name__)
