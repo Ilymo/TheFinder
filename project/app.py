53ba33f33b9e9d316f1fe7387d7e9e59ee6cb46a
Ilymo <118273904+Ilymo@users.noreply.github.com>
2024-02-14 13:14:18 +0000

    Wed, Feb 14, 2024, 2:14 PM +01:00

--- project/app.py
+++ project/app.py
@@ -31,7 +31,7 @@ def result():
                 tag2 = request.args.get("tag2")
                 year = request.args.get("year")
                 rate = request.args.get("rate")
-                movie = db.execute("SELECT name FROM movies WHERE released_at >= ? LIMIT 10", year)
+                movie = db.execute("SELECT * FROM movies WHERE released_at >= ? LIMIT 10", year)
                 print("tag1:",tag1,"tag2:",tag2,"year:",year,"rate:",rate)
                 return render_template("result.html", movie = movie)
 
