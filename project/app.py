ffdbcaba9af1992ce913c9f4f10f0fb7669d683c
Ilymo <118273904+Ilymo@users.noreply.github.com>
2024-02-15 09:52:57 +0000

    Thu, Feb 15, 2024, 10:52 AM +01:00

--- project/app.py
+++ project/app.py
@@ -65,6 +65,8 @@ def result():
         print("genre:", tag1, "year:", year, "rate:", rate)
         unique_words = set(tag1.split())
         print(unique_words)
+        movie = db.execute("SELECT * FROM movies WHERE Release_Date >= ? AND Vote_Average >= ? AND Genre LIKE ? LIMIT 10",
+                           year, rate, tag1)
     return render_template("result.html")
 
     # if no input
