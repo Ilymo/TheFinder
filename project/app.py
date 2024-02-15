272f6c7306f58ffce590f579a751375f7bd2eca4
Ilymo <118273904+Ilymo@users.noreply.github.com>
2024-02-15 08:39:08 +0000

    Thu, Feb 15, 2024, 9:39 AM +01:00

--- project/app.py
+++ project/app.py
@@ -38,14 +38,15 @@ def result():
         tag3 = request.args.get("tag3")
         year = request.args.get("year")
         rate = request.args.get("rate")
+        # Sqlite query with tag1, tag2, tag3, year, rate
         movie = db.execute("SELECT * FROM movies WHERE Release_Date >= ? AND Vote_Average > ? AND Genre LIKE ? AND Genre LIKE ? AND Genre LIKE ? LIMIT 10",
                            year, rate, (f'%{tag1}%'), (f'%{tag2}%'), (f'%{tag3}%'))
         print("tag1:", tag1, "tag2:", tag2, "year:", year, "rate:", rate)
         print("movie:", movie)
-        # If no result:
+        # if no result:
         if not movie:
             return render_template("noresult.html")
         # if result
         return render_template("result.html", movie=movie)
-
+    # if no input
     return render_template("noresult.html")
