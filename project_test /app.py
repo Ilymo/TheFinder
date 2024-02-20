d379fff23ec1bf0768e79bdbb8a7693766384a04
Ilymo <118273904+Ilymo@users.noreply.github.com>
2024-02-20 07:55:54 +0000

    Tue, Feb 20, 2024, 8:55 AM +01:00

--- project_test /app.py
+++ project_test /app.py
@@ -12,7 +12,24 @@ ANIME_TAGS = ["Action", "Adventure", "Avant Garde", "Award Winning", "Boys Love"
 app = Flask(__name__)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///media.db")
+import sqlite3
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+def get_db_connection():
+    conn = sqlite3.connect('media_test.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
+
 
 
 @app.route("/")
